8781a9d4ef1239a7b0e26ea71e44d1d21f08dc25
Pradnya Chandrakant Bhosale <67332759+PRADNYA199@users.noreply.github.com>
2024-09-02 10:28:36 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function stringChop(str, size) {
   // your code here
+	for(str==NULL || str!=='string' || size<=0 )
+		{
+		return [];
+		}
+	let chunk=[];
+	for(let i=0;i<str.length;i=+size)
+		{
+			chunk.push(str.slice(i,i+size))
+		}
+	return chunk;
 }
 
 // Do not change the code below
